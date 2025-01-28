44f80dde71a9faf1544a995bf52b7fb3a9515deb
Geert Meersman <geertmeersman+github@gmail.com>
2025-01-28 17:07:33 +0100

    feat: add version to User-Agent

--- custom_components/eeveemobility/const.py
+++ custom_components/eeveemobility/const.py
@@ -10,8 +10,17 @@ PLATFORMS: Final = [Platform.SENSOR, Platform.DEVICE_TRACKER]
 
 ATTRIBUTION: Final = "Data provided by Eevee Mobility"
 
+manifestfile = Path(__file__).parent / "manifest.json"
+with open(manifestfile) as json_file:
+    manifest_data = json.load(json_file)
+
+DOMAIN = manifest_data.get("domain")
+NAME = manifest_data.get("name")
+VERSION = manifest_data.get("version")
+ISSUEURL = manifest_data.get("issue_tracker")
+
 CUSTOM_HEADERS = {
-    "User-Agent": "Home Assistant Eevee Mobility Integration",
+    "User-Agent": f"Home Assistant Eevee Mobility Integration {VERSION}",
 }
 
 DEFAULT_SCAN_INTERVAL = 15
@@ -22,14 +31,6 @@ WEBSITE = "https://eeveemobility.com/"
 EVENTS_LIMIT = 50
 EVENTS_EXCLUDE_KEYS = ["polyline", "graphs"]
 
-manifestfile = Path(__file__).parent / "manifest.json"
-with open(manifestfile) as json_file:
-    manifest_data = json.load(json_file)
-
-DOMAIN = manifest_data.get("domain")
-NAME = manifest_data.get("name")
-VERSION = manifest_data.get("version")
-ISSUEURL = manifest_data.get("issue_tracker")
 STARTUP = f"""
 -------------------------------------------------------------------
 {NAME}
